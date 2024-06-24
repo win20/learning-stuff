0a5c36c08ae8505a683c561c058ae8e1d7519c24
Win Barua <win.barua2@gmail.com>
2024-06-24 23:26:41 +0100

    nextjs-dashboard: setup postgres DB

--- nextjs-dashboard/app/lib/placeholder-data.js
+++ nextjs-dashboard/app/lib/placeholder-data.js
@@ -86,7 +86,7 @@ const invoices = [
     date: '2023-08-05',
   },
   {
-    customer_id: customers[7].id,
+    customer_id: customers[2].id,
     amount: 54246,
     status: 'pending',
     date: '2023-07-16',
